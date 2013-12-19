4484236dbe94df3166f2299c6ebbb3c5b0df9807
OpenShift guest <te222ds@student.lnu.se>
2013-12-19 15:46:17 -0500

    memory halvfärdig

--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -29,7 +29,52 @@ Message.prototype.getHTMLText = function() {
 };
 
 Message.prototype.getDateText = function() {
-    return this.getDate();
+    var month = this.getDate().getMonth();
+    var year = this.getDate().getFullYear();
+    var day = this.getDate().getDate();
+    var hours = this.getDate().getHours();
+    var minutes = this.getDate().getMinutes();
+    var secounds = this.getDate().getSeconds();
+    
+    if(month === 0){
+        month = "Januari";
+    }
+    if(month == 1){
+        month = "Februari";
+    }
+    if(month == 2){
+       month = "Mars";
+    }
+    if(month == 3){
+        month = "April";
+    }
+    if(month == 4){
+        month = "Maj";
+    }
+    if(month == 5){
+        month = "Juni";
+    }
+    if(month == 6){
+       month = "Juli";
+    }
+    if(month == 7){
+        month = "Augusti";
+    }
+    if(month == 8){
+        month = "September";
+    }
+    if(month == 9){
+        month = "Oktober";
+    }
+    if(month == 10){
+        month = "November";
+    }
+    if(month == 11){
+        month = "December";
+    }
+    
+    return "Inlägget skapades den "+day+" "+month+" "+year+" Klockan "+hours+":"+minutes+":"+secounds;
+
 };
 
 //Hämtar tiden med bara timmar och minuter.
--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -5,17 +5,17 @@ var Memory = {
     cols: 4,
     rows: 4,
     
+    picsave: [],
     random: [],
     
     init:function(e){
         
-    var picturearray = new RandomGenerator.getPictureArray(Memory.cols, Memory.rows);
-    Memory.random.push(picturearray);
+    Memory.random = new RandomGenerator.getPictureArray(Memory.cols, Memory.rows);
     
     Memory.tablesize();
     },
     tablesize: function(){
-        
+        var cellid = 0;
         var div = document.getElementById("memory");
         var table = document.createElement("table");
         div.appendChild(table);
@@ -26,21 +26,66 @@ var Memory = {
             var tr = document.createElement("tr");
             
                 for(var c = 0; c < Memory.cols; c+=1){
-                    var td = document.createElement("td");
-                tr.appendChild(td);
-                
-                var img0 = document.createElement("img");
-                img0.setAttribute("src", "../pics/0.png");
-                td.appendChild(img0);
-                
+                    tr.appendChild(Memory.creattd(cellid));
+                    cellid+=1;
+                    
             }
             tbody.appendChild(tr);
             
         }
+    },
+    creattd: function(cellid){
         
-    }
-    
-    
+        var td = document.createElement("td");
+                    var img0 = document.createElement("img");
+                    var a = document.createElement("a");
+                    
+                    img0.setAttribute("src", "../pics/0.png");
+                    td.appendChild(a);
+                    a.appendChild(img0);
+                    
+                        a.onclick = function(){
+                            if(img0.getAttribute("src") == "../pics/0.png"){
+                                Memory.flip(cellid);
+                            }
+                        };
+                             
+                    
+                    
+                    img0.setAttribute("id", cellid);
+        return td;
+    },
+    changepic: function(cellid){
+        var img = document.getElementById(cellid);
+        if(img.getAttribute("src") == "../pics/0.png"){
+            img.setAttribute("src", "../pics/" +Memory.random[cellid] +".png");
+            Memory.picsave.push(cellid);
+                    
+        }
+        else{
+            setTimeout(function() {
+                img.setAttribute("src", "../pics/0.png");
+            }, 500);
+        }
+
+        
+    },
+    flip: function(cellid){
+        Memory.changepic(cellid);
+        if(Memory.picsave.length == 2){
+            if(Memory.random[Memory.picsave[0]] == Memory.random[Memory.picsave[1]]){
+                setTimeout(function() {
+                alert("Rätt!");    
+                }, 500);
+                
+            }
+            else{
+                    Memory.changepic(Memory.picsave[0]);
+                    Memory.changepic(Memory.picsave[1]); 
+            } 
+             Memory.picsave.length = 0;
+        }
+    },
     
 };
 window.onload = Memory.init;
