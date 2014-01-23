897c157ec6b25b64c3bae405c9e1e2a4d056629c
Tim <te222ds@student.lnu.se>
2014-01-23 16:03:11 +0100

    Projekt börjat

---
+++ 5-projekt/app.js
@@ -0,0 +1,52 @@
+"use strict";
+var desktop = {
+	
+	init:function(){
+		
+		var button = document.querySelector("#knapp");
+		
+		button.onclick = function(){
+			var popup = document.createElement("div");
+			popup.setAttribute("id", "popup");
+			var main = document.getElementById("main");
+			main.appendChild(popup);
+			
+			var top = document.createElement("div");
+			top.setAttribute("id", "top");
+			var mid = document.createElement("div");
+			mid.setAttribute("id", "mid");
+			var bottom = document.createElement("div");
+			bottom.setAttribute("id", "bottom");
+			
+			popup.appendChild(top);
+			popup.appendChild(mid);
+			popup.appendChild(bottom);
+			
+			var topright = document.createElement("div");
+			topright.setAttribute("id", "topright");
+			
+			var topleft = document.createElement("div");
+			topleft.setAttribute("id", "topleft");
+			
+			top.appendChild(topleft);
+			top.appendChild(topright);
+			
+			var desktopname = document.createTextNode("Image Viewer");
+			
+			var buttonremove = document.createElement("button");
+            buttonremove.setAttribute("id", "avbryt");
+            buttonremove.appendChild(document.createTextNode("X"));
+			
+			topleft.appendChild(desktopname);
+			topright.appendChild(buttonremove);
+		}
+		
+	},
+	
+	imageview:function(){
+		
+		
+		
+	}
+};
+window.onload = desktop.init;
